cdab4b202b4801f494bc520966d1f805ee2c2867
Claudine Lamothe <claudine.lamothe@mindgeek.com>
2016-10-03 18:08:53 -0400

    Added example config file and added missing npm packages

---
+++ config-default.js
@@ -0,0 +1,6 @@
+// replace the values in this file with your own and rename the file to config.js
+var config = {};
+
+config.mongodb = "mongodb://<dbuser>:<dbpassword>@ds035806.mlab.com:35806/<dbname>";
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var config = require('./config');
-var express  = require('express');
-var app      = express();                        // create our app w/ express
+var express = require('express');
+var app = express();                        // create our app w/ express
 var mongoose = require('mongoose');              // mongoose for mongodb
 var morgan = require('morgan');                  // log requests to the console (express4)
 var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-
 
 mongoose.connect(config.mongodb);
 
