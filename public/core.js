8590565622e0067e3ba3c2ea3aca47eb1fea1a3e
Clo <Clo>
2016-10-05 22:06:13 -0400

    Improving popup

--- public/core.js
+++ public/core.js
@@ -55,10 +55,10 @@ function quizzController($scope, $http) {
       navigator.serviceWorker.register('sw.js').then(function() {
         return navigator.serviceWorker.ready;
       }).then(function(reg) {
-        console.log('Service Worker is ready :^)', reg);
-        if ('serviceWorker' in navigator) { console.log('Service Worker is supported'); navigator.serviceWorker.register('sw.js').then(function() { return navigator.serviceWorker.ready; }).then(function(reg) { console.log('Service Worker is ready :^)', reg); reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) { console.log('endpoint:', sub.endpoint); }); }).catch(function(error) { console.log('Service Worker error :^(', error); }); }
+        console.log('Service Worker is ready', reg);
+        if ('serviceWorker' in navigator) { console.log('Service Worker is supported'); navigator.serviceWorker.register('sw.js').then(function() { return navigator.serviceWorker.ready; }).then(function(reg) { console.log('Service Worker is ready :^)', reg); reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) { console.log('endpoint:', sub.endpoint); }); }).catch(function(error) { console.log('Service Worker error', error); }); }
       }).catch(function(error) {
-        console.log('Service Worker error :^(', error);
+        console.log('Service Worker error', error);
       });
     }
     
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,3 @@
-console.log('Started', self);
-
 self.addEventListener('install', function(event) {
   self.skipWaiting();
   console.log('Installed', event);
@@ -10,15 +8,20 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('push', function(event) {
-  console.log('Push message', event);
 
-  var title = 'Push message';
+  var question = {};
+  
+  var title = 'Qux quizz - A new question begs to be answered!';
 
   event.waitUntil(
     self.registration.showNotification(title, {
-     body: 'The Message',
+     body: question.text,
      icon: 'images/icon.png',
-     tag: 'my-tag'
+     tag: 'quizz',
+     actions: [
+      { "action": "yes", "title": "Yes", "icon": "images/yes.png" },
+      { "action": "no", "title": "No", "icon": "images/no.png" }
+    ]
    }));
 });
 
@@ -27,26 +30,6 @@ self.addEventListener('notificationclick', function(event) {
   // Android doesn't close the notification when you click it
   // See http://crbug.com/463146
   event.notification.close();
-  var url = 'https://youtu.be/gYMkEMCHtJ4';
-  // Check if there's already a tab open with this URL.
-  // If yes: focus on the tab.
-  // If no: open a tab with the URL.
-  event.waitUntil(
-    clients.matchAll({
-      type: 'window'
-    })
-    .then(function(windowClients) {
-      console.log('WindowClients', windowClients);
-      for (var i = 0; i < windowClients.length; i++) {
-        var client = windowClients[i];
-        console.log('WindowClient', client);
-        if (client.url === url && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(url);
-      }
-    })
-  );
+  
+  // TODO: go to notification
 });
