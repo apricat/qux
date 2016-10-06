b5992582eca69dbe9acc1f5274b3004f2a064f42
Clo <Clo>
2016-10-05 21:00:34 -0400

    Adding service workers

--- public/core.js
+++ public/core.js
@@ -50,6 +50,18 @@ function mainController($scope, $http) {
 }
 
 function quizzController($scope, $http) {
+    if ('serviceWorker' in navigator) {
+      console.log('Service Worker is supported');
+      navigator.serviceWorker.register('sw.js').then(function() {
+        return navigator.serviceWorker.ready;
+      }).then(function(reg) {
+        console.log('Service Worker is ready :^)', reg);
+        if ('serviceWorker' in navigator) { console.log('Service Worker is supported'); navigator.serviceWorker.register('sw.js').then(function() { return navigator.serviceWorker.ready; }).then(function(reg) { console.log('Service Worker is ready :^)', reg); reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) { console.log('endpoint:', sub.endpoint); }); }).catch(function(error) { console.log('Service Worker error :^(', error); }); }
+      }).catch(function(error) {
+        console.log('Service Worker error :^(', error);
+      });
+    }
+    
     const ERROR = 0;
     const SUCCESS = 1;
 
